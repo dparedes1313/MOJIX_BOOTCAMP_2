1f536f39dc35a39532051d74f4d7fa84d866a7f0
Danny Paredes <111035467+dparedes1313@users.noreply.github.com>
2022-08-13 10:23:26 -0500

    up2

--- Programa1.py
+++ Programa1.py
@@ -1,11 +1,19 @@
 import streamlit as st   
 st.title("10 Cool Beginner Python Tricks That Will Make Your Life Easier") 
-st.header ("Simple but effective tips for every python lovers")
+st.subheader ("Simple but effective tips for every python lovers")
 st.image ('https://miro.medium.com/max/700/1*5IFgojJ4nU8f0YKTcjWDrg.jpeg')
+st.caption("The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can surprise you with their amazing capabilities.
+
+In today’s article, I will discuss 10 Python tips and tricks that will be really helpful for beginners to write more compact code. Knowing these tips and tricks will definitely save you some valuable time.")
+
+
+
+
+
 st.header("this is the markdown")
 st.markdown("this is the header")
 st.subheader("this is the subheader")
-st.caption("this is the caption")
+
 st.code("x=2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 st.checkbox('yes')
