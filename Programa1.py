427bb15c4127286b1ee8e35fa115233e13d80f0c
Danny Paredes <111035467+dparedes1313@users.noreply.github.com>
2022-08-13 10:31:25 -0500

    up5

--- Programa1.py
+++ Programa1.py
@@ -3,11 +3,21 @@ st.title("10 Cool Beginner Python Tricks That Will Make Your Life Easier")
 st.header ("Simple but effective tips for every python lovers")
 st.image ('https://miro.medium.com/max/700/1*5IFgojJ4nU8f0YKTcjWDrg.jpeg')
 st.caption("Photo by Miesha Maiden from Pexels")
-
 st.header("this is the markdown")
 st.markdown("this is the header")
 st.subheader("this is the subheader")
+import pandas as pd
+import numpy as np
+import altair as alt
 
+df = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+
+c = alt.Chart(df).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+
+st.write(c)
 st.code("x=2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 st.checkbox('yes')
