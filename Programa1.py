a2967abca5184101d1e177c0636cbcfda7c5d759
Danny Paredes <111035467+dparedes1313@users.noreply.github.com>
2022-08-13 12:07:42 -0500

    10

--- Programa1.py
+++ Programa1.py
@@ -9,33 +9,24 @@ st.header('1. Walrus operator')
 st.write("<h2><p>The&nbsp;<code><strong>Walrus</strong></code><strong>&nbsp;or&nbsp;</strong><code><strong>:=</strong></code>&nbsp;operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.</p></h2>", unsafe_allow_html=True)
 st.header('Example')
 st.write("<h2><p>If we want to check and print the length of a list:</p></h2>", unsafe_allow_html=True)
-st.code("Mylist = [1,2,3]")
-st.code("if(l := len(mylist) &gt; 2)")
-st.code("print(l)")
+code = '''Mylist = [1,2,3]
+if(l := len(mylist) > 2)
+print(l)'''
 st.write("<h2><p>Output</p></h2>", unsafe_allow_html=True)
 st.code("3")
 st.header('2. Splitting a string')
 st.write("<h2><p>If you want to split the components of a string into a list you can do that easily using the split() function in python. This will make the string operations a lot easier!<p></h2>", unsafe_allow_html=True)
 st.header('Example')
-st.code("string = “hello world”")
-st.code("string.split()")
-st.code("print(l)")
+code = '''string = “hello world”
+string.split()'''
 st.write("<h2><p>Output</p></h2>", unsafe_allow_html=True)
 st.code("[‘hello’, ‘world’]")
 st.header('3. Reversing a string')
 st.write("<h2><p>If you want to reverse a given string, you can do that with only one line of code using the negative indexing of the string.<p></h2>", unsafe_allow_html=True)
 st.header('Example')
-st.code("[‘hello’, ‘world’]")
-st.code("a=str[::-1]")
-st.code('print(a)")')
-st.write("<h2><p>Output</p></h2>", unsafe_allow_html=True)
-st.code("[!dlrow olleh]")
-st.header('4. Merging two dictionaries')
-st.write("<h2><p>This amazing trick will help you merge two dictionaries with just 1 line of code. We just need to use ** in front of the name of the two dictionaries like below two merge them into a single dictionary:<p></h2>", unsafe_allow_html=True)
-st.header('Example')
-st.code("[‘hello’, ‘world’]")
-st.code("a=str[::-1]")
-st.code('print(a)")')
+code = '''str=”hello world!”
+a=str[::-1]
+print(a)'''
 st.write("<h2><p>Output</p></h2>", unsafe_allow_html=True)
 st.code("[!dlrow olleh]")
 st.header('4. Merging two dictionaries')
@@ -45,7 +36,29 @@ code = '''d1 = {“a”: 10, “b”:20}
 d2 = {“c”: 30, “d”:40}
 d3 = {**d1, **d2}
 print(d3)'''
+st.write("<h2><p>Output</p></h2>", unsafe_allow_html=True)
+st.code("{‘a’: 10, ‘b’: 20, ‘c’: 30, ‘d’: 40}")
+st.header('The zip() function')
+st.write("<h2><p>The zip() function in python can make your life a lot easier when working with lists and dictionaries. It is used to combine several lists of the same length.<p></h2>", unsafe_allow_html=True)
+st.header('Example')
+code = '''colour = [“red”, “yellow”, “green”]
+fruits = [‘apple’, ‘banana’, ‘mango’]
+for colour, fruits in zip(colour, fruits):
+print(colour, fruits)'''
 st.code(code, language='python')
 st.write("<h2><p>Output</p></h2>", unsafe_allow_html=True)
-st.code("[!dlrow olleh]")
+code = '''red apple
+yellow banana
+green mango'''
+st.code(code, language='python')
+st.write("<h2><p>The zip() function can also be used for combining two lists into a dictionary. This method can be really helpful while grouping data from the list.<p></h2>", unsafe_allow_html=True)
+st.header('Example')
+code = '''students = [“Rajesh”, “kumar”, “Kriti”]
+marks = [87, 90, 88]
+dictionary = dict(zip(students, marks))
+print(dictionary)'''
+st.code(code, language='python')
+st.write("<h2><p>Output</p></h2>", unsafe_allow_html=True)
+code = '''{‘Rajesh’: 87, ‘kumar’: 90, ‘Kriti’: 88}'''
+st.code(code, language='python')
 
