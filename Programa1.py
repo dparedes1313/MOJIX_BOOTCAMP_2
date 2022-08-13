9eb31e2c11fdc32f1ab70fb593c2d93427b3c949
Danny Paredes <111035467+dparedes1313@users.noreply.github.com>
2022-08-13 12:36:18 -0500

    100

--- Programa1.py
+++ Programa1.py
@@ -125,5 +125,5 @@ Wrong password'''
 st.code(code, language='python')
 
 st.header('Conclusion')
-st.write("<h2><p>These were a few amazing Python tips and tricks which will make your work a lot easier while coding. There are many more shortcuts like these that you can explore from the official documentation or any other website. Note: This article contains an affiliate link. This means that if you click on it and choose to buy the resource I linked above, a small portion of your subscription fee will go to me. However, the recommended resource is experienced by me and helped me in my data science career journey.<p></h2>", unsafe_allow_html=True)
+st.write("<h2><p><p>These were a few amazing Python tips and tricks which will make your work a lot easier while coding. There are many more shortcuts like these that you can explore from the official documentation or any other website.</p><p><strong><em>Note:&nbsp;</em></strong><em>This article contains an affiliate link. This means that if you click on it and choose to buy the resource I linked above, a small portion of your subscription fee will go to me.</em></p><p><em>However, the recommended resource is experienced by me and helped me in my data science career journey.</em></p><p></h2>", unsafe_allow_html=True)
 
