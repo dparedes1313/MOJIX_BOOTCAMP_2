5973eea875a21d527120705615ee656c39a91681
Danny Paredes <111035467+dparedes1313@users.noreply.github.com>
2022-08-13 19:08:50 -0500

    122

--- Programa1.py
+++ Programa1.py
@@ -1,6 +1,6 @@
 import streamlit as st   
 st.sidebar.title("10 Cool Beginner Python Tricks That Will Make Your Life Easier") 
-st.sidebar.buton('1. Walrus operator')
+st.sidebar.header('1. Walrus operator')
 st.sidebar.header('2. Splitting a string')
 st.sidebar.header('3. Reversing a string')
 st.sidebar.header('4. Merging two dictionaries')
