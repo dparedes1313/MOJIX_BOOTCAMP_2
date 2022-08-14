c35bc96d419456b572d22beb15289fa02705c650
Danny Paredes <111035467+dparedes1313@users.noreply.github.com>
2022-08-13 23:19:45 -0500

    106

--- Programa1.py
+++ Programa1.py
@@ -1,6 +1,18 @@
 import streamlit as st  
 ####SIDE BAR ################################################################# 
-st.sidebar.title("10 Cool Beginner Python Tricks That Will Make Your Life Easier") 
+st.sidebar.title("10 Cool Beginner Python Tricks That Will Make Your Life Easier")
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    ) 
 #st.sidebar.checkbox('contenido')
 #st.sidebar.checkbox('contacto')
 page_names = ['Contenido','Contacto']
