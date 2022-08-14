903074d6df5ec64990e655f937914d0402935b23
Danny Paredes <111035467+dparedes1313@users.noreply.github.com>
2022-08-13 22:23:17 -0500

    105

--- Programa1.py
+++ Programa1.py
@@ -1,6 +1,6 @@
 import streamlit as st   
 st.sidebar.title("10 Cool Beginner Python Tricks That Will Make Your Life Easier") 
-st.st.checkbox('YES')
+st.sidebar.checkbox('yes')
 st.sidebar.header('1. Walrus operator')
 st.sidebar.button('click')
 st.sidebar.header('2. Splitting a string')
