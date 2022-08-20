7302c88f3fc59ec96420893cc01d0415e128c2f2
Danny Paredes <111035467+dparedes1313@users.noreply.github.com>
2022-08-19 23:25:34 -0500

    COMMITSIDEBAR

--- proyecto2.py
+++ proyecto2.py
@@ -1,4 +1,25 @@
 import streamlit as st
+####SIDE BAR ################################################################# 
+st.sidebar.title("100 Cool Beginner Python Tricks That Will Make Your Life Easier")
+st.sidebar.header('1. Walrus operator')
+st.sidebar.header('2. Splitting a string')
+st.sidebar.header('3. Reversing a string')
+st.sidebar.header('4. Merging two dictionaries')
+st.sidebar.header('5.The zip() function')
+st.sidebar.header('6. Assigning multiple list values to a variable')
+st.sidebar.header('7. Remove duplicate list items')
+st.sidebar.header('8. Lambda function')
+st.sidebar.header('9. Swapping variable value')
+st.sidebar.header('10. Use a password in your code')
+     
+st.sidebar.title("Contact")
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+####MAIN PAGE #################################################################
+##### CARGA DE DATOS CSV ##############################
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
      # To read file as bytes:
@@ -16,3 +37,4 @@ if uploaded_file is not None:
      # Can be used wherever a "file-like" object is accepted:
      dataframe = pd.read_csv(uploaded_file)
      st.write(dataframe)
+###################################################################
