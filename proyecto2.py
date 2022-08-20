e6608acf080e768e4cbe8302e0aea4fa9c48cc70
Danny Paredes <111035467+dparedes1313@users.noreply.github.com>
2022-08-19 23:47:16 -0500

    6

--- proyecto2.py
+++ proyecto2.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import streamlit as st
-
+st.dataframe(df, 200, 100)
 st.title("Hello world!")
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
   st.write(dataframe)
-  st.dataframe(df, 200, 100)
