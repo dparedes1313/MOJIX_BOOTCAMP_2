2a7ffd03f1b153a3d20fe02f931e079b46b4519a
Danny Paredes <111035467+dparedes1313@users.noreply.github.com>
2022-08-20 11:56:27 -0500

    1

--- proyecto2.py
+++ proyecto2.py
@@ -9,4 +9,4 @@ for uploaded_file in uploaded_files:
      bytes_data = uploaded_file.read()
      st.write("filename:", uploaded_file.name)
      st.write(bytes_data)
-     st.write(df)
+     st.write(df())
