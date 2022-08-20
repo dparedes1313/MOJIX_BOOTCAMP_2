ccbeec82b0b374571bdf773f69262b3a2deeff41
Danny Paredes <111035467+dparedes1313@users.noreply.github.com>
2022-08-20 12:00:48 -0500

    1

--- proyecto2.py
+++ proyecto2.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+array = np.array([[50, 20], [50, 20]]) 
 import streamlit as st
 
 st.title("Hello world!")
@@ -9,7 +10,7 @@ for uploaded_file in uploaded_files:
      bytes_data = uploaded_file.read()
      st.write("filename:", uploaded_file.name)
      st.write(bytes_data)
-     
+
      df = pd.DataFrame(
      np.random.randn(50, 20),
      columns=('col %d' % i for i in range(20)))
