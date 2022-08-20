a4a2b5811283cd48fcf46e8e96c7d99ddfa0fab9
Danny Paredes <111035467+dparedes1313@users.noreply.github.com>
2022-08-20 11:55:24 -0500

    1

--- proyecto2.py
+++ proyecto2.py
@@ -4,7 +4,9 @@ import streamlit as st
 
 st.title("Hello world!")
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
-  st.write(df)
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+     bytes_data = uploaded_file.read()
+     st.write("filename:", uploaded_file.name)
+     st.write(bytes_data)
+     st.write(df)
