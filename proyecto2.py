8f93e83776ec6d446c31b8800fae60d706e2e4aa
Danny Paredes <111035467+dparedes1313@users.noreply.github.com>
2022-08-20 08:22:39 -0500

    1

--- proyecto2.py
+++ proyecto2.py
@@ -11,7 +11,7 @@ for uploaded_file in uploaded_files:
     
 
     df = pd.DataFrame(
-    np.random.randn(10, 20),
+    np.random.randn(50, 20),
     columns=('col %d' % i for i in range(20)))
 
-st.dataframe(df.style.highlight_max(axis=0))
+st.dataframe(df)  # Same as st.write(df)
